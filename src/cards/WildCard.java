8556121f31dc29934f3282a12da50a4254dc4ea0
AbdelhadiZA <zabilaabdelhadi76@gmail.com>
2026-01-28 11:06:55 +0100

    Refactor WildCard class to remove abstract modifier

--- src/cards/WildCard.java
+++ src/cards/WildCard.java
@@ -3,7 +3,7 @@ package cards;
 import enumTypes.Color;
 import game.GameSession;
 
-public abstract class WildCard extends Card {
+public class WildCard extends Card {
     protected Color chosenColor=Color.BLACK;
     public void setColor(Color color) {
         this.chosenColor = color;
@@ -21,4 +21,5 @@ public abstract class WildCard extends Card {
 }
 
 		
+
 
